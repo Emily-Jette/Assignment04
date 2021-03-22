cdc1f3f3c2d722abc278d91d7c6a6953b1547264
Emily Jette <emily.jette@ocsbstudent.ca>
2021-03-22 17:14:07 +0000

    created new pages and added 4 images

---
+++ script.js
@@ -0,0 +1,6 @@
+/* trying to create button that will go to nextpage; not working try to fix later
+document.getElementById('gallery').addEventListener('click', newPage)
+
+function newPage () {
+}
+*/
