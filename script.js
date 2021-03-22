c83d86a527ecba2bd5f2dcb49201678d99b29bff
Emily Jette <emily.jette@ocsbstudent.ca>
2021-03-22 17:52:44 +0000

    buttons go to pages, facts, and images half done

--- script.js
+++ script.js
@@ -1,6 +1,12 @@
-/* trying to create button that will go to nextpage; not working try to fix later
+// These buttons lead from index.html to index2 and index3
 document.getElementById('gallery').addEventListener('click', newPage)
 
 function newPage () {
+  window.location = 'https://assignment04.emilyjette.repl.co/index2.html'
 }
-*/
+
+document.getElementById('fact-page').addEventListener('click', nextPage)
+
+function nextPage () {
+  window.location ='https://assignment04.emilyjette.repl.co/index3.html'
+}
