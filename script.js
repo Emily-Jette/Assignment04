389b916bf12eb23493637def7aa8956cc6586a79
Emily Jette <emily.jette@ocsbstudent.ca>
2021-03-23 18:03:24 +0000

    did more stuff

--- script.js
+++ script.js
@@ -1,11 +1,11 @@
 // These buttons lead from index.html to index2 and index3
 document.getElementById('gallery').addEventListener('click', newPage)
 
-function newPage () {
-  window.location = 'https://assignment04.emilyjette.repl.co/index2.html'
-}
-
 document.getElementById('fact-page').addEventListener('click', nextPage)
+
+function newPage () {
+  window.location = 'https://assignment04.emilyjette.repl.co/gallery.html'
+}
 
 function nextPage () {
   window.location = 'https://assignment04.emilyjette.repl.co/index3.html'
