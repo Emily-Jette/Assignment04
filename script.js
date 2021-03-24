8e60c048b8044c9ffd10580ff6f9148f6c44bc7f
Emily Jette <emily.jette@ocsbstudent.ca>
2021-03-24 17:31:21 +0000

    finished gallery page

--- script.js
+++ script.js
@@ -1,17 +1,17 @@
 // These buttons lead from index.html to index2 and index3
 document.getElementById('gallery').addEventListener('click', newPage)
 
-document.getElementById('fact-page').addEventListener('click', nextPage)
-
-document.getElementById('home').addEventListener('click', backPage)
-
 function newPage () {
   window.location = 'https://assignment04.emilyjette.repl.co/gallery.html'
 }
 
+document.getElementById('fact-page').addEventListener('click', nextPage)
+
 function nextPage () {
   window.location = 'https://assignment04.emilyjette.repl.co/index3.html'
 }
+
+document.getElementById('home').addEventListener('click', backPage)
 
 function backPage () {
   window.location = 'https://assignment04.emilyjette.repl.co/'
